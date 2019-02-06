0fbce7b754bb355704ea860bf81c3843546d01ab
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-02-06 10:13:53 +0100

    Add UUID trait to Field model

--- app/Field.php
+++ app/Field.php
@@ -6,9 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Field extends Model
 {
+    use Traits\UsesUuid;
+
     //Kobling til flere components
     public function components(){
         $this->belongsToMany('App\Component', 'component_id');
     }
 }
-        
