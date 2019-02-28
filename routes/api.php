a5f3fed87927d5d57c1b97bd2cb5f970797f86ae
bereketgadhanom <bereketgadhanom@gmail.com>
2019-02-28 12:57:43 +0100

    API Routes registrering

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,39 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:api')->get('/pages', function (Request $request) {
-    return $request->pages();
-});
+
+
+// Pages
+Route::get('/pages', 'PageController@index');
+Route::get('pages/{page}', 'PageController@show');
+
+// Links
+Route::get('/links', 'LinkController@index');
+Route::get('/links/{link}', 'LinkController@show');
+
+// Menus
+Route::get('/menus', 'MenuController@index');
+Route::get('/menus/{menu}', 'MenuController@show');
+
+// Menu_locations
+Route::get('/menu_locations', 'MenuLocationController@index');
+Route::get('/menu_locations/{menu_location}', 'MenuLocationController@show');
+
+// Components
+Route::get('/components', 'ComponentController@index');
+Route::get('/components/{component}',  'ComponentController@show');
+
+// Fields
+Route::get('/fields', 'FieldController@index');
+Route::get('/fields/{field}', 'FieldController@show');
+
+// Images og Image_size
+Route::get('/images', 'ImageController@index');
+Route::get('/images/{image}', 'FieldController@show');
+
+Route::get('/image_sizes', 'ImageController@index');
+Route::get('/images_sizes/{image_size}', 'ImageController@show');
+
+// Users
+Route::get('/users', 'UserController@index');
+Route::get('/users/{user}', 'UserController@show');
