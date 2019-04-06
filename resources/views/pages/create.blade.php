b8c411c2a99b7e0a85657aa2f272bd91885f0ef1
Bjørnar Hagen <post@bjornarhagen.no>
2019-04-06 23:13:35 +0200

    Remove duplicate code

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -8,32 +8,6 @@
 
         @include('pages.form-fields')
 
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-             @csrf
-                <strong>Tittel:</strong>
-                <input type="text" name="title" value="{{old('title', (isset($page->title)? $page->title : null))}}" required class="form-control" placeholder="Tittel">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Bilde ID:</strong>
-                <select name="image_id"  class="form-control" >
-                    <option></option>
-                    @foreach($images as $image)
-                    <option value="{{$image->id}}" {{old('image_id', $image->id)}}? selected> {{$image->url}} </option>
-                    @endforeach
-                </select>
-
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary"> Opprett </button>
-        </div>
-    </div>
-   
-</form>
         <button type="submit">Opprett</button>
     </form>
 
