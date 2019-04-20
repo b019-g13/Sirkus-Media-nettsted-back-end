59d6d477ffae83ec4639ee6094842299a83ea295
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-04-20 17:35:16 +0200

    Update create link modal. Add edit and delete modals for links in menu views

--- resources/views/links/edit-modal.blade.php
+++ resources/views/links/edit-modal.blade.php
@@ -1,13 +1,14 @@
-<div id="menu-modal-create-link" class="modal modal-small">
+<div id="menu-modal-edit-link" class="modal modal-small">
     <header class="modal-header">
         <button type="button" class="button-action button-blank modal-closer">
             @icon('x')
         </button>
-        <span class="modal-heading">Lag en link</span>
+        <span class="modal-heading">Rediger link</span>
     </header>
     <section class="modal-body">
-        <form action="{{ route('links.store') }}" method="POST">
+        <form action="{{ route('links.update', 'LINK_ID') }}" method="POST">
             @csrf
+            @method('patch')
             @include('links.form-fields')
         </form>
     </section>
--- resources/views/menus/edit.blade.php
+++ resources/views/menus/edit.blade.php
@@ -19,6 +19,8 @@
     </form>
 
     @include('links.create-modal')
+    @include('links.edit-modal')
+    @include('links.delete-modal')
 
     <script src="{{ asset('js/menu.js') }}" defer></script>
 @endsection
