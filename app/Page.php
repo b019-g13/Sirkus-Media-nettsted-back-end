db4d58832dc067b9331254057c39768747d87363
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-04-21 01:37:32 +0200

    Disable storing values in link_id until links are properly setup

--- app/Page.php
+++ app/Page.php
@@ -169,7 +169,8 @@ class Page extends Model
             if ($field->type === 'image' && !empty($field->value)) {
                 $page_component->image_id = $field->value;
             } else if ($field->type === 'url_internal' && !empty($field->value)) {
-                $page_component->link_id = $field->value;
+                // $page_component->link_id = $field->value;
+                $page_component->value = $field->value;
             } else {
                 $page_component->value = $field->value;
             }
