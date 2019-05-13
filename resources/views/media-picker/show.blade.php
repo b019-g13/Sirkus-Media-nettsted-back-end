e5b63f4474a13109779e871b38031715e71f1d7e
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-05-13 15:30:28 +0200

    Update media picker page to show the media picker

--- resources/views/media-picker/show.blade.php
+++ resources/views/media-picker/show.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
     @include('media-picker.show-content')
+    <script>
+        (function() {
+            document.body.style.overflow = 'hidden';
+            document.querySelector('main').style.padding = '15em 0';
+        })();
+    </script>
     <script src="{{ asset('js/media-picker.js') }}"></script>
-    <script>setupMediaPicker();</script>
+    <script>
+        setTimeout(() => {
+            window.pageMediaPicker = new mediaPicker();
+
+            window.addEventListener("media-picker-ready", function() {
+
+                window.pageMediaPicker.functions.show();
+                window.pageMediaPicker.elements.submitButton.style.display = 'none';
+            });
+        }, 500);
+    </script>
 @endsection
